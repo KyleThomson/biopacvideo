82aaebddc4747df79ec653a5599122c07382685f
kylethomson@gmail.com <kylethomson@gmail.com@8cfd434f-9575-0c91-5abc-5438b077098d>
2012-07-25 21:14:37 +0000

    new stuff

---
+++ trunk/Playback2/MyLibVLC/OpenFrm.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SeizurePlayback
+{
+    public partial class OpenFrm : Form
+    {         
+        public OpenFrm(string FN, string Rvwr, string N, double PC, int FileLength, DateTime LR, DateTime LO)
+        {
+            InitializeComponent();
+            FileLabel.Text = "File Name: " + FN;
+            Reviewing.Checked = true;
+            Reviewer.Text = Rvwr;
+            NotesLabel.Text = "Notes: " + N;
+            if (PC == 0)
+            {                
+                PercentLabel.Text = "";
+                LastReviewLabel.Text = "File has not been reviewed.";
+            }
+            else if (Math.Ceiling(PC) >= 100)
+            {
+                PercentLabel.Text = "Review Complete";
+                LastReviewLabel.Text = "Last reviewed on " + LR.ToLongDateString();
+                Reviewing.Checked = false; 
+            }
+            else
+            {
+                TimeSpan t = TimeSpan.FromSeconds((int)((PC * FileLength)/100));
+                string answer = string.Format("{0:D2}:{1:D2}:{2:D2}", t.Hours, t.Minutes, t.Seconds);
+                PercentLabel.Text = "Stopped at " + answer; 
+                LastReviewLabel.Text = "Last reviewed on " + LR.ToShortDateString();
+            }
+            if (LO == DateTime.MinValue)
+            {
+                LastOpenLabel.Text = "File has never been opened.";
+            }
+            else
+            {
+                LastOpenLabel.Text = "Last opened on " + LO.ToLongDateString();
+            }
+        }
+        public string GetReviewer()
+        {
+            return Reviewer.Text;
+        }
+        public bool GetReviewing()
+        {
+            return Reviewing.Checked;   
+        }
+        private void OpenFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OKbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
