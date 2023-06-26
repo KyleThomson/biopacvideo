daa2ce970e5ce14d7d541e9004219af97f4984b2
Sarah Haslam <u1317214@umail.utah.edu>
2023-06-26 14:46:41 -0600

    Defined version as 2.0, added version to ini file

--- BioPacVideo/MainForm.cs
+++ BioPacVideo/MainForm.cs
@@ -248,6 +248,7 @@ namespace BioPacVideo
         {
             BioIni.IniWriteValue("General", "RecDirectory", MP.RecordingDirectory);
             BioIni.IniWriteValue("General", "Unit Name", UnitLabel);
+            BioIni.IniWriteValue("General", "Version", 2.0);
             //videofix qualifier added
             BioIni.IniWriteValue("BioPac", "VideoFixDisabled", true);
             BioIni.IniWriteValue("BioPac", "MPType", MP.MPtype);
