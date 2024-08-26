2901d241d859dd5a04e45a3a98f7dfd5fda34370
Ben Erekson <157845778+Reesesben2@users.noreply.github.com>
2024-08-26 17:23:33 -0600

    Adjust Format and spacingon advanced Feeder control

--- BioPacVideo/AdvancedFeederControl.Designer.cs
+++ BioPacVideo/AdvancedFeederControl.Designer.cs
@@ -29,35 +29,56 @@
         private void InitializeComponent()
         {
             this.CommandValue = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxHistory = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // CommandValue
             // 
-            this.CommandValue.Location = new System.Drawing.Point(75, 43);
+            this.CommandValue.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CommandValue.Location = new System.Drawing.Point(12, 429);
             this.CommandValue.Name = "CommandValue";
-            this.CommandValue.Size = new System.Drawing.Size(100, 20);
+            this.CommandValue.Size = new System.Drawing.Size(646, 20);
             this.CommandValue.TabIndex = 0;
+            this.CommandValue.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CommandValue_KeyPressed);
             // 
-            // button1
+            // buttonSend
             // 
-            this.button1.Location = new System.Drawing.Point(75, 78);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(138, 106);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Square Edge Button For Ben";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(664, 424);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(110, 29);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxHistory
+            // 
+            this.textBoxHistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxHistory.Enabled = false;
+            this.textBoxHistory.Location = new System.Drawing.Point(12, 12);
+            this.textBoxHistory.Multiline = true;
+            this.textBoxHistory.Name = "textBoxHistory";
+            this.textBoxHistory.ReadOnly = true;
+            this.textBoxHistory.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHistory.Size = new System.Drawing.Size(762, 406);
+            this.textBoxHistory.TabIndex = 2;
             // 
             // AdvancedFeederControl
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(281, 228);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(786, 461);
+            this.Controls.Add(this.textBoxHistory);
+            this.Controls.Add(this.buttonSend);
             this.Controls.Add(this.CommandValue);
             this.Name = "AdvancedFeederControl";
             this.Text = "AdvancedFeederControl";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,6 +87,7 @@
         #endregion
 
         private System.Windows.Forms.TextBox CommandValue;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxHistory;
     }
 }
--- BioPacVideo/AdvancedFeederControl.cs
+++ BioPacVideo/AdvancedFeederControl.cs
@@ -13,20 +13,54 @@ namespace BioPacVideo
     public partial class AdvancedFeederControl : Form
     {
         FeederTemplate Feeder;
+        int CurrentIndex = -1;
+
         public AdvancedFeederControl()
         {
             InitializeComponent();
             Feeder = FeederTemplate.Instance;
+            this.AcceptButton = buttonSend;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonSend_Click(object sender, EventArgs e)
         {
             if (int.TryParse(CommandValue.Text, out int result))
             {
                 if ((result < 0) || (result > 31))
                     return;
                 Feeder.SendDirectCommand((byte)result);
+                textBoxHistory.Text = CommandValue.Text + Environment.NewLine + textBoxHistory.Text;
+                CommandValue.Text = "";
             }
+        }
+
+        private void CommandValue_KeyPressed(object sender, KeyEventArgs e)
+        {
+            if (textBoxHistory.Lines.Length > 0)
+            {
+                if (e.KeyCode == Keys.Up && CurrentIndex < textBoxHistory.Lines.Count() - 2) //Count adds 1 to array and Text box always has a newline.
+                {
+                    CurrentIndex += 1;
+                    fillFromHistory();
+                }
+                else if (e.KeyCode == Keys.Down && CurrentIndex > -1)
+                {
+                    CurrentIndex -= 1;
+                    fillFromHistory();
+                }
+            }
+        }
+
+        private void fillFromHistory()
+        {
+            if (CurrentIndex == -1)
+            {
+                CommandValue.Text = "";
+            } else if (CurrentIndex < textBoxHistory.Lines.Length)
+            {
+                CommandValue.Text = textBoxHistory.Lines[CurrentIndex];
+                CommandValue.SelectionStart = CommandValue.Text.Length;
+            }            
         }
     }
 }
